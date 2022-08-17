a41f74c3636812543c71a7fc08cbd17d2a08bab5
= <=>
2022-08-17 01:52:23 -0300

    added toaster on form - added required fields in form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,7 +1,7 @@
 
 import emailjs from "emailjs-com";
 import data from "../data"; //It is an object with the 3 variables (service, template and user id) needed to use emailjs
-
+import toast, {Toaster} from "react-hot-toast";
 
 function Form() {
 
@@ -9,28 +9,29 @@ function Form() {
     e.preventDefault();
 
     emailjs.sendForm(data.serviceId, data.templateId, e.target, data.userId).then((result) =>{
-      console.log(result.text)
+      toast.success("Email sent!");
  ;   }, (error) => {
-  console.log(error.text);
+  toast.error(error.text);
  });
     e.target.reset();
    }
 
   return (
       <div className="form-container">
+        <Toaster />
         <div className="h1-box"><h1>Contact me</h1></div>
         <form onSubmit={sendEmail}>
           <div className="form-input name">
-            <input type="text" placeholder="Name" name="name"/>
+            <input type="text" placeholder="Name" name="name" required/>
           </div>          
           <div className="form-input email">
-            <input type="email" placeholder="Email Address" name="email"/>
+            <input type="email" placeholder="Email Address" name="email" required/>
             </div>          
           <div className="form-input subject">
-            <input type="text" placeholder="Subject" name="subject" />
+            <input type="text" placeholder="Subject" name="subject" required />
           </div>          
           <div className="form-input message">
-            <textarea name="message" id="" cols="30" rows="10" placeholder="Your message">
+            <textarea name="message" id="" cols="30" rows="10" placeholder="Your message" required>
             </textarea></div>
           <div className="btn-box">
             <input className="form-btn" type="submit" value="Send Message" />
